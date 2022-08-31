0532e70e48122ad290084ed89790888f2abb4a61
AndreevDan93 <andreevdan93@gmail.com>
2022-08-31 16:05:40 +0500

    add correct logic

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -8,10 +8,12 @@ import java.util.TreeSet;
 
 public class Differ {
     static String generate(String filePath1, String filePath2, String format) throws Exception {
-        Map<String, Object> map1 = JsonFormat.fileToMap(FileSearcher.getAbsolutePathToFile(filePath1));
-        Map<String, Object> map2 = JsonFormat.fileToMap(FileSearcher.getAbsolutePathToFile(filePath2));
+
+        Map<String, Object> map1 = Parser.fileToMap(filePath1);
+        Map<String, Object> map2 = Parser.fileToMap(filePath2);
 
         Set<String> keysSet = new TreeSet<>();
+
         keysSet.addAll(map1.keySet());
         keysSet.addAll(map2.keySet());
 
@@ -24,7 +26,16 @@ public class Differ {
             } else if (!map1.containsKey(key) && map2.containsKey(key)) {
                 builder.append("  + ").append(key).append(": ").append(map2.get(key)).append("\n");
             } else if (map1.containsKey(key) && map2.containsKey(key)) {
-                if (map1.get(key).equals(map2.get(key))) {
+                if (map1.get(key) == null || map2.get(key) == null) {
+                    if (map1.get(key) == null && map2.get(key) == null) {
+                        builder.append("    ").append(key).append(": ").append(map1.get(key)).append("\n");
+                    } else {
+                        builder.append("  - ").append(key).append(": ").append(map1.get(key)).append("\n");
+                        builder.append("  + ").append(key).append(": ").append(map2.get(key)).append("\n");
+                    }
+                }
+
+                else if (map1.get(key).equals(map2.get(key))) {
                     builder.append("    ").append(key).append(": ").append(map1.get(key)).append("\n");
                 } else {
                     builder.append("  - ").append(key).append(": ").append(map1.get(key)).append("\n");
--- src/main/java/hexlet/code/Parser.java
+++ src/main/java/hexlet/code/Parser.java
@@ -1,9 +1,25 @@
 package hexlet.code;
 
+import hexlet.code.formates.JsonFormat;
+import hexlet.code.formates.YmlFormat;
+
 import java.io.File;
 import java.nio.file.Path;
+import java.util.Map;
 
-public class FileSearcher {
+public class Parser {
+    public static Map<String,Object> fileToMap(String path) throws Exception {
+        if (path.endsWith("json")) {
+            Map<String, Object> map = JsonFormat.fileToMap(getAbsolutePathToFile(path));
+            return map;
+        }
+        if (path.endsWith("yml")) {
+            Map<String, Object> map = YmlFormat.fileToMap(getAbsolutePathToFile(path));
+            return map;
+        }
+        throw new RuntimeException("this file type is not supported");
+    }
+
     public static Path getAbsolutePathToFile(String inputPathString) {
         Path absolutePath;
         if (inputPathString.charAt(0) == '/') {
