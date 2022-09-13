2d70019f10be57c6be5601c7412efb6cb9211ace
AndreevDan93 <andreevdan93@gmail.com>
2022-09-13 11:38:53 +0500

    add fixes after review

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -8,34 +8,34 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Differ {
     private static final String LOCAL_PATH_INSIDE_PROJECT = "src/main/resources/";
 
     public static String generate(String path1, String path2, String format) throws Exception {
-        Path filePath1 = getAbsolutePathToFile(path1);
-        Path filePath2 = getAbsolutePathToFile(path2);
-        Map<String, Value> dataFileMap = Parser.getDataFile(filePath1, filePath2);
-        return switch (format) {
-            case "json" -> JsonFormatter.format(dataFileMap);
-            case "plain" -> PlainFormatter.format(dataFileMap);
-            case "stylish" -> StylishFormatter.format(dataFileMap);
-            default -> throw new RuntimeException(format + " this format is not supported");
-        };
+        Path absolutePath1 = getAbsolutePathToFile(path1);
+        Path absolutePath2 = getAbsolutePathToFile(path2);
+        Map<String, Object> mapFromFile1 = Parser.fileToMap(absolutePath1);
+        Map<String, Object> mapFromFile2 = Parser.fileToMap(absolutePath2);
+        Map<String, Value> mapOfChanges = getMapOfChanges(mapFromFile1, mapFromFile2);
+        return formatting(mapOfChanges, format);
     }
 
     public static String generate(String path1, String path2) throws Exception {
         return generate(path1, path2, "stylish");
     }
 
-    public static Path getAbsolutePathToFile(String inputPathString) throws IOException {
+    private static Path getAbsolutePathToFile(String inputPathString) throws IOException {
         Path inputPath = Path.of(inputPathString);
         Path absolutePath;
         if (inputPath.isAbsolute() || new File(inputPath.toString()).exists()) {
             return inputPath;
         } else {
-            StringBuilder absolutePathSB = new StringBuilder(inputPath.toAbsolutePath().toAbsolutePath().toString());
+            StringBuilder absolutePathSB = new StringBuilder(inputPath.toAbsolutePath().toString());
             absolutePathSB.insert(absolutePathSB.length() - inputPathString.length(), LOCAL_PATH_INSIDE_PROJECT);
             absolutePath = Path.of(absolutePathSB.toString());
         }
@@ -44,5 +44,33 @@ public class Differ {
         }
         return absolutePath;
     }
+
+    private static Map<String, Value> getMapOfChanges(Map<String, Object> map1, Map<String, Object> map2) {
+        Map<String, Value> mapOfChanges = new LinkedHashMap<>();
+        Set<String> keysSet = new TreeSet<>();
+        keysSet.addAll(map1.keySet());
+        keysSet.addAll(map2.keySet());
+        String isNotInitialized = Value.IS_NOT_INITIALIZED;
+        for (String key : keysSet) {
+            if (map1.containsKey(key) && map2.containsKey(key)) {
+                mapOfChanges.put(key, new Value(map1.get(key), map2.get(key)));
+            } else if (!map1.containsKey(key)) {
+                mapOfChanges.put(key, new Value(isNotInitialized, map2.get(key)));
+            } else {
+                mapOfChanges.put(key, new Value(map1.get(key), isNotInitialized));
+            }
+        }
+        return mapOfChanges;
+    }
+
+    private static String formatting(Map<String, Value> mapOfChanges, String format) throws Exception {
+        return switch (format) {
+            case "json" -> JsonFormatter.format(mapOfChanges);
+            case "plain" -> PlainFormatter.format(mapOfChanges);
+            case "stylish" -> StylishFormatter.format(mapOfChanges);
+            default -> throw new RuntimeException(format + " this format is not supported");
+        };
+    }
+
 
 }
--- src/main/java/hexlet/code/Parser.java
+++ src/main/java/hexlet/code/Parser.java
@@ -5,52 +5,31 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.nio.file.Path;
-import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+
 
 import static java.nio.file.Files.readString;
 
 public class Parser {
-    public static Map<String, Value> getDataFile(Path path1, Path path2) throws Exception {
-        Map<String, Object> map1 = fileToMap(path1);
-        Map<String, Object> map2 = fileToMap(path2);
-        Map<String, Value> fileChangesMap = new LinkedHashMap<>();
-        Set<String> keySet = getKeySetFromTwoMaps(map1, map2);
-        String isNotInitialized = Value.IS_NOT_INITIALIZED;
-        for (String key : keySet) {
-            if (map1.containsKey(key) && map2.containsKey(key)) {
-                fileChangesMap.put(key, new Value(map1.get(key), map2.get(key)));
-            } else if (!map1.containsKey(key)) {
-                fileChangesMap.put(key, new Value(isNotInitialized, map2.get(key)));
-            } else {
-                fileChangesMap.put(key, new Value(map1.get(key), isNotInitialized));
-            }
-        }
-        return fileChangesMap;
-    }
 
     public static Map<String, Object> fileToMap(Path path) throws Exception {
         String pathString = path.toString();
-        if (pathString.endsWith("json")) {
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(readString(path), new TypeReference<>() {
-            });
-        }
-        if (pathString.endsWith("yml")) {
-            ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-            return mapper.readValue(readString(path), new TypeReference<>() {
-            });
-        }
-        throw new RuntimeException("this file type is not supported");
-    }
+        String extension = pathString.substring(pathString.lastIndexOf(".") + 1);
+        switch (extension) {
+            case "json" -> {
+                ObjectMapper mapper = new ObjectMapper();
+                return mapper.readValue(readString(path), new TypeReference<>() {
+                });
+            }
+            case "yml" -> {
+                ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+                return mapper.readValue(readString(path), new TypeReference<>() {
+                });
+            }
+            default -> throw new RuntimeException("this file type is not supported");
 
-    public static Set<String> getKeySetFromTwoMaps(Map<String, Object> map1, Map<String, Object> map2) {
-        Set<String> keysSet = new TreeSet<>();
-        keysSet.addAll(map1.keySet());
-        keysSet.addAll(map2.keySet());
-        return keysSet;
+        }
     }
-
 }
+
+
