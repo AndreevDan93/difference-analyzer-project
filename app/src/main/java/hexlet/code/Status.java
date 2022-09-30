60ef24b2c00f73dcada70e0d49e2a976d0063e36
AndreevDan93 <andreevdan93@gmail.com>
2022-09-30 12:00:10 +0500

    fixed after chek mentor

--- app/src/main/java/hexlet/code/Status.java
+++ app/src/main/java/hexlet/code/Status.java
@@ -1,8 +1,18 @@
 package hexlet.code;
 
 public enum Status {
-    STATUS_UNCHANGED,
-    STATUS_CHANGED,
-    STATUS_ADDED,
-    STATUS_DELETED
+    STATUS_UNCHANGED("status:unchanged"),
+    STATUS_CHANGED("status:changed"),
+    STATUS_ADDED("status:added"),
+    STATUS_DELETED("status:deleted");
+
+    private final String value;
+
+    Status(String title) {
+        this.value = title;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
--- app/src/main/java/hexlet/code/formatter/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatter/JsonFormatter.java
@@ -4,34 +4,49 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.Value;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 
 final class JsonFormatter extends Formatter {
 
+    private final List<Map<String, Object>> diffList = new LinkedList<>();
+
     @Override
+
     public String format(Map<String, Value> valueMap) throws JsonProcessingException {
+        generateFormat(valueMap);
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.writeValueAsString(valueMap);
+        return mapper.writeValueAsString(diffList);
     }
 
     @Override
     protected void valueWasAdded(Map<String, Value> valueMap, String key) {
-        throw new RuntimeException("unsupported");
+        String value = key + ":" + valueMap.get(key).getSecondOb();
+        diffList.add(Map.of(getKeyDiffList(valueMap, key), value));
     }
 
     @Override
     protected void valueWasDeleted(Map<String, Value> valueMap, String key) {
-        throw new RuntimeException("unsupported");
+        String value = key + ":" + valueMap.get(key).getFirstOb();
+        diffList.add(Map.of(getKeyDiffList(valueMap, key), value));
     }
 
     @Override
     protected void valueWasChanged(Map<String, Value> valueMap, String key) {
-        throw new RuntimeException("unsupported");
+        String value = key + ":" + valueMap.get(key).getFirstOb() + "," + valueMap.get(key).getSecondOb();
+        diffList.add(Map.of(getKeyDiffList(valueMap, key), value));
     }
 
     @Override
     protected void valueWasUnchanged(Map<String, Value> valueMap, String key) {
-        throw new RuntimeException("unsupported");
+        String value = key + ":" + valueMap.get(key).getFirstOb();
+        diffList.add(Map.of(getKeyDiffList(valueMap, key), value));
     }
+
+    private String getKeyDiffList(Map<String, Value> valueMap, String key) {
+        return valueMap.get(key).getStatus().getValue();
+    }
+
 }
